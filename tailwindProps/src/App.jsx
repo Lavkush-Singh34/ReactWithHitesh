8e9b1c134daf80a9c4529112d47279b2f3b3c57e
lavkush <lavkushsingh1998@gmail.com>
2024-05-04 23:24:24 +0530

    learnt tailwind and props

---
+++ tailwindProps/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    fName: 'lavkush',
+    lName: 'singh'
+  }
+
+  return (
+    <>
+    <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind and React</h1>
+    <Card username="Lavkush" someObj={myObj} btnText="Click Me!"/>
+    <Card username="chaiaurcode" />
+    </>
+  )
+}
+
+
+export default App
